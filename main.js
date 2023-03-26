73be93495c8595d1eca394766d996c73895d05af
antoanstin <antoanstin@mail.com>
2023-03-26 21:56:24 +0530

    add

---
+++ main.js
@@ -0,0 +1,25 @@
+				let employees = [
+				{
+				firstName: 'John',
+				lastName: 'Doe',
+				age: 27,
+				joinedDate: 'December 15, 2017'
+				},
+				{
+				firstName: 'Ana',
+				lastName: 'Rosy',
+				age: 25,
+				joinedDate: 'January 15, 2019'
+				},
+				{
+				firstName: 'Zion',
+				lastName: 'Albert',
+				age: 30,
+				joinedDate: 'February 15, 2011'
+				}
+				];
+				let d=employees.sort(function(a,b)
+				{
+					return a.age-b.age;
+				});
+				console.log(d);
